08cdfc1e0a39072726e8ef7cdd3602e8ebc6719e
Jung Hyun, Nam <rkttu@rkttu.com>
2022-09-06 17:26:05 +0900

    Chocolatey nuspec 초안 추가

---
+++ Chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/yourtablecloth/TableCloth/releases/download/v0.5.4/TableCloth_0.5.4_Setup.msi'
+$url64      = $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'TableCloth*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
